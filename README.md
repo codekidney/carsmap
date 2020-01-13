384207d123a4f5b3bd0fe868a9bbfcb5a2e1d6e8
Kamil Nowak <info@kamilnowak.com>
2020-01-13 20:01:33 +0100

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# carsmap
-Cars Map
---
+++ precache-manifest.d77a748baa5baabf6509971b444742f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24dca389fdf8a6a0a79190aecf6ac9f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aea15294a9abb6516acb",
+    "url": "/static/css/2.098bc035.chunk.css"
+  },
+  {
+    "revision": "d62a2bd39f88ecfadaef",
+    "url": "/static/css/main.2d590ca6.chunk.css"
+  },
+  {
+    "revision": "aea15294a9abb6516acb",
+    "url": "/static/js/2.1766d621.chunk.js"
+  },
+  {
+    "revision": "5d902a352ffd255227de11674b8784c8",
+    "url": "/static/js/2.1766d621.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d62a2bd39f88ecfadaef",
+    "url": "/static/js/main.0679c26d.chunk.js"
+  },
+  {
+    "revision": "70030ac3e12c87239c49",
+    "url": "/static/js/runtime-main.4336489d.js"
+  }
+]);
